d84f44510fd5e82324222356f109c5d6026e8c1e
F2A_Jung_Lucas <l.jung@ludus-academie.com>
2016-10-21 21:53:54 +0200

    no message

--- main.c
+++ main.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define TAILLE 10
+
 int main()
 {
-
 int larg=0;
 int lon=0;
 int choixLarg=0;
@@ -16,21 +15,30 @@ printf("entrez une valeur largeur : \n");
 scanf("%d",&choixLarg);
 
 //Boucles
-for(lon=0;lon<=choixLon;lon++){
-        printf("0");
-        for (larg=0;larg<=choixLarg;larg++){
 
-    if(lon==1 || lon==choixLon || larg==1 || larg==choixLarg){
+//boucle de 0 en haut
+        for (larg=1;larg<=choixLarg+2;larg++){
+                printf("0");}
+        printf("\n");
 
-        printf("*");
- }
-        else { printf("*");}
 
+    for(lon=1;lon<=choixLon;lon++){
+//boucle de 0 àgauche
+        for(lon=1;lon<=choixLon;lon++){ printf("0");
+        for (larg=1;larg<=choixLarg;larg++){printf("*");
         }
-printf("\n");
-
-
+//boucle de 0 àdroite
+        if(larg==choixLarg){printf("0");}
+        else{printf("0");}printf("\n");
+        }
 }
+//boucle de 0 en bas
+        for (larg=1;larg<=choixLarg+2;larg++){
+                printf("0");}
+        printf("\n");
 
     return 0;
 }
+
+
+
